9d92d4c6eaa9e839d063e711eb33bf2b4deb7c1e
jiangqianghua <jiangqianghua@jiangqianghuadeMacBook-Pro.local>
2018-07-01 13:36:02 +0800

    first commit

---
+++ repository/src/test/java/com/jqh/gradletest/RepositoryTest.java
@@ -0,0 +1,13 @@
+package com.jqh.gradletest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RepositoryTest {
+
+    @Test
+    public void testSave(){
+        String a = "a";
+        Assert.assertNotNull(a);
+    }
+}
---
+++ src/main/java/com/jqh/App.java
@@ -0,0 +1,9 @@
+package com.jqh;
+
+public class App {
+
+    //java -classpath groovy-1.0-SNAPSHOT.jar com.jqh.App
+    public static void main(String[] args) {
+        System.out.println("app run");
+    }
+}
